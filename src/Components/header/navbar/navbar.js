89656dd4beea31a7e4855722a7f14bbbf640ea9d
Nitsuga <88418234+Agusst1n@users.noreply.github.com>
2021-12-13 00:27:03 -0300

    segundo commit

--- src/Components/header/navbar/navbar.js
+++ src/Components/header/navbar/navbar.js
@@ -2,17 +2,21 @@ import React from 'react';
 import './navbar.css';
 import Logo from "../../../assets/logo.png"
 function Navbar() {
+  
   return (
     <div className="contendorNav">
       <div className='logo'>
           <img src={Logo}/>
       </div>
-      {/* <div className='navbar'>
+      <div className='navbar'>
         <button>Nosotros</button>
         <button>Menu</button>
         <button>Contactanos</button>
         <button>Carrito</button>
-      </div> */}
+      </div>
+      <div className='btns'>
+        <ion-icon name="menu-outline"></ion-icon>
+      </div>
     </div>
   );
 }
