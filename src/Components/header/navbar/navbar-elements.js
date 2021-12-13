be7972151c0cc78a65f3eb3a09a3efb4fc8577ad
Nitsuga <88418234+Agusst1n@users.noreply.github.com>
2021-12-13 12:58:47 -0300

    tercer commit

---
+++ src/Components/header/navbar/navbar-elements.js
@@ -0,0 +1,69 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 90%;
+    border:solid 1px violet;
+    margin:auto;
+`;
+
+export const Wrapper = styled.div`
+    width:90%;
+    display:flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+`;
+
+export const LogoContainer = styled.div``;
+
+export const Menu = styled.ul`
+    display:flex;
+    justify-content: space-between;
+    list-style:none;
+
+
+    @media (max-width:768px){
+        position:fixed;
+        top:-16px;
+        left:${({open})=> open ? "0" : "-100%"}; //Si OPEN es falso su valor va a ser -100% pero si es verdadero valdra 0
+        width:50%;
+        height:100vh;
+        z-index:2;
+        background-color:violet;
+        display:flex;
+        flex-direction:column;
+        justify-content:space-around;
+        align-items:center;
+        transition: 0.5s all ease;
+    }
+`;
+
+export const MenuItems = styled.li`
+    display:flex;
+    justify-content: space-between;
+    align-items:center;
+`;
+
+export const MenuLink = styled.a`
+    color:white;
+    margin-right:25px;
+    cursor:pointer;
+
+    &:hover{
+        color:violet;
+    }
+`;
+
+export const MobileIcon = styled.div`
+    display:none;
+
+    @media (max-width:768px){
+        display:flex;
+        align-items:center;
+        justify-content:center
+    }
+
+    svg{
+        fill: #e07924;
+        margin-right:0.5rem;
+    }
+`;
--- src/Components/header/navbar/navbar.js
+++ src/Components/header/navbar/navbar.js
@@ -1,23 +1,48 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './navbar.css';
 import Logo from "../../../assets/logo.png"
-function Navbar() {
+import {Container, LogoContainer, Menu, MenuItems, MenuLink, MobileIcon, Wrapper} from './navbar-elements'
+import { FaAlignRight } from "react-icons/fa";
+const Navbar = () => {
   
+  const [showMobileMenu, setShowMobileMenu]= useState(false);
+
+
   return (
-    <div className="contendorNav">
-      <div className='logo'>
+    <Container>
+      <Wrapper>
+        <LogoContainer className='logo'>
           <img src={Logo}/>
-      </div>
-      <div className='navbar'>
-        <button>Nosotros</button>
-        <button>Menu</button>
-        <button>Contactanos</button>
-        <button>Carrito</button>
-      </div>
-      <div className='btns'>
-        <ion-icon name="menu-outline"></ion-icon>
-      </div>
-    </div>
+        </LogoContainer>
+
+          <MobileIcon onClick={() => setShowMobileMenu(!showMobileMenu)}>
+            <FaAlignRight></FaAlignRight>
+          </MobileIcon>
+
+        <Menu open={showMobileMenu}>
+          <MenuItems>
+            <MenuLink>
+              Home          
+            </MenuLink>
+          </MenuItems>
+          <MenuItems>
+            <MenuLink>
+              About          
+            </MenuLink>
+          </MenuItems>
+          <MenuItems>
+            <MenuLink>
+              Menu          
+            </MenuLink>
+          </MenuItems>
+          <MenuItems>
+            <MenuLink>
+              Carrito          
+            </MenuLink>
+          </MenuItems>
+        </Menu>
+      </Wrapper>
+    </Container>
   );
 }
 
